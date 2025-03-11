87f1895794ffc888b696dbe04167da90e2f68a5f
snowpeak <xuefeng@snowpeak.org>
2025-03-11 15:20:59 +0800

    [06: Create a persona and test again](https://microsoft.github.io/TechExcel-Integrating-Azure-PaaS-and-AI-Services-for-AI-Design-Wins/docs/05_enhance_api_with_semantic_kernel/05_02.html#05-test-the-agent)

--- src/ContosoSuitesWebAPI/Agents/MaintenanceCopilot.cs
+++ src/ContosoSuitesWebAPI/Agents/MaintenanceCopilot.cs
@@ -13,7 +13,14 @@ namespace ContosoSuitesWebAPI.Agents
     {
         // Exercise 5 Task 2 TODO #3: Uncomment the two lines below to declare the Kernel and ChatHistory objects.
         public readonly Kernel _kernel = kernel;
-        private ChatHistory _history = new();
+         private ChatHistory _history = new ("""
+     You are a friendly assistant who likes to follow the rules. You will complete required steps
+     and request approval before taking any consequential actions, such as saving the request to the database.
+     If the user doesn't provide enough information for you to complete a task, you will keep asking questions
+     until you have enough information to complete the task. Once the request has been saved to the database,
+     inform the user that hotel maintenance has been notified and will address the issue as soon as possible.
+     """);
+
 
         /// <summary>
         /// Chat with the maintenance copilot.
